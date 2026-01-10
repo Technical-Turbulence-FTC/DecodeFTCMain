82c3c83262e6de8f967e26fe6d824322f67d64f6
DanTheMan-byte <danibarrf@gmail.com>
2026-01-09 22:21:03 -0600

    stash

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LimelightTest.java
@@ -1,14 +1,18 @@
 package org.firstinspires.ftc.teamcode.tests;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import java.util.List;
 
+@Config
+@TeleOp
 //TODO: fix to get the apriltag that it is reading
 public class LimelightTest extends LinearOpMode {
     MultipleTelemetry TELE;
